5927f25407781a93c429b6274e0ffefff674b930
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-08-16 18:58:12 +0700

    fix: resolve android build errors by checking keystore properties existence

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,10 +46,12 @@ android {
 
    signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
