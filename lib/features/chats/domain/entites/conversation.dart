1e970625c33d31cbef508308ed21c89c0f59e81b
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-08-23 18:59:12 +0700

    feat(chats): add copyWith helper to Conversation entity

--- lib/features/chats/domain/entites/conversation.dart
+++ lib/features/chats/domain/entites/conversation.dart
@@ -23,6 +23,28 @@ class Conversation extends Equatable {
     required this.unreadCountPatient,
   });
 
+  Conversation copyWith({
+    String? id,
+    List<String>? participants,
+    String? name,
+    LastMessage? lastMessage,
+    String? status,
+    int? unreadCountDoctor,
+    int? unreadCountPatient,
+    List<UnReadInfo>? unreadInfo,
+  }) {
+    return Conversation(
+      id: id ?? this.id,
+      participants: participants ?? this.participants,
+      name: name ?? this.name,
+      lastMessage: lastMessage ?? this.lastMessage,
+      status: status ?? this.status,
+      unreadCountDoctor: unreadCountDoctor ?? this.unreadCountDoctor,
+      unreadCountPatient: unreadCountPatient ?? this.unreadCountPatient,
+      unreadInfo: unreadInfo ?? this.unreadInfo,
+    );
+  }
+
   @override
   List<Object?> get props => [
     id,
